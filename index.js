e4efbd1b5cdd8b36bd928d44974dfb10c7276b5b
Michael Moreno <mmoreno2222@gmail.com>
2020-08-30 21:38:40 -0700

    fixed error in initial file structure

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
+const generateMarkdown = require("./utils/generateMarkdown")
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        name: "title",
 
+    }
 ];
 
 // function to write README file
